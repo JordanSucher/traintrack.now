c8da75f87c6861a15c49bd641b6dab294b65db0d
JordanSucher <jsucher@gmail.com>
2025-03-07 14:55:19 -0500

    trying to figure out python serverless fns

--- api/index.py
+++ api/index.py
@@ -1,17 +1,10 @@
-# Place this file at /api/index.py (at root level)
-
 from http.server import BaseHTTPRequestHandler
-
-class Handler(BaseHTTPRequestHandler):
+ 
+class handler(BaseHTTPRequestHandler):
+ 
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
+        self.send_header('Content-type','text/plain')
         self.end_headers()
         self.wfile.write('Hello, world!'.encode('utf-8'))
         return
-
-def handler(request, response):
-    return {
-        'statusCode': 200,
-        'body': 'Hello, world!'
-    }
