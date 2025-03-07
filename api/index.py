c2b938d883507742ab1ef9372f8e0b25e51cdf82
JordanSucher <jsucher@gmail.com>
2025-03-07 16:03:04 -0500

    testing reports serverless

--- api/index.py
+++ api/index.py
@@ -1,10 +1,18 @@
 from http.server import BaseHTTPRequestHandler
- 
+from gtfs.fetch_reports import fetch_reports
+import os
+
 class handler(BaseHTTPRequestHandler):
  
     def do_GET(self):
+
+        beaconId = os.environ.get("BEACON_ID")
+
+        reports = fetch_reports(beaconId)
+        print(reports)
+
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
+        self.wfile.write(str(reports).encode('utf-8'))
         return
