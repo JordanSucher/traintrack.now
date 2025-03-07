faf9b8bd890e563c5ec49066016d9f8ed7f5e212
JordanSucher <jsucher@gmail.com>
2025-03-07 14:50:47 -0500

    trying to figure out python serverless fns

--- api/index.py
+++ api/index.py
@@ -1,9 +1,17 @@
-# /api/index.py
-from fastapi import FastAPI, Request
-from fastapi.responses import PlainTextResponse
+# Place this file at /api/index.py (at root level)
 
-app = FastAPI()
+from http.server import BaseHTTPRequestHandler
 
-@app.get("/")
-def handler(request: Request):
-    return PlainTextResponse("Hello, world!")
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write('Hello, world!'.encode('utf-8'))
+        return
+
+def handler(request, response):
+    return {
+        'statusCode': 200,
+        'body': 'Hello, world!'
+    }
